587fa79fc2057d6720b2782d8945cb5a8d144f37
Sachin Alam <sachinalam4216@gmail.com>
2023-12-09 10:05:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const apiURL = "https://api.quotable.io/random";
+
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+async function getQuote(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+}
+
+getQuote(apiURL);
+
+function tweet() {
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + " --- by " + author.innerHTML, "Tweet Window", "width=600, height=300");
+}
